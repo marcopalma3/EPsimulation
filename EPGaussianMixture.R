6126e1c98bf2e6266b385d6f6d6378263da0a257
marcopalma3 <mapalm3@hotmail.it>
2018-02-07 13:58:42 +0000

    Functions for mixtures

--- EPGaussianMixture.R
+++ EPGaussianMixture.R
@@ -40,10 +40,10 @@ EPGM = function(n,x,iter,likelihood_var = 1,w){
       mu_star = Sigma_star %*% (solve(Sigma__i) %*% mu__i + matrix(c(likelihood_var,0,0,0),2,2) %*% matrix(c(x[i],0),2,1))
       
       Sigma_hat = solve(solve(Sigma__i) + matrix(c(0,0,0,likelihood_var),2,2))
-      mu_hat = Sigma_star %*% (solve(Sigma__i) %*% mu__i + matrix(c(0,0,0,likelihood_var),2,2) %*% matrix(c(0,x[i]),2,1))
+      mu_hat = Sigma_hat %*% (solve(Sigma__i) %*% mu__i + matrix(c(0,0,0,likelihood_var),2,2) %*% matrix(c(0,x[i]),2,1))
       
       mu_prime = w * mu_star + (1-w) * mu_hat
-      Sigma_prime = w * Sigma_star + (1-w) * Sigma_hat + w*(1-w)* (mu_star - mu_hat) %*% t(mu_star - mu_hat)
+      Sigma_prime = w * Sigma_star + (1-w) * Sigma_hat + (mu_star - mu_hat) %*% t(mu_star - mu_hat)
       
       mu = mu_prime
       Sigma = Sigma_prime
@@ -69,17 +69,46 @@ mu_prior = c(0,0)
 var_prior = c(1,1)
 theta1 = rnorm(1,mu_prior[1],var_prior[1])
 theta2 = rnorm(1,mu_prior[2],var_prior[2])
-w = sample(c(1,0),n,replace = TRUE)
-theta = c(theta1,theta2)
-x = w * rnorm(1,theta1,1) + (1-w) * rnorm(1,theta2,1)
+theta1 = -3
+theta2 = 3
+pi = sample(c(1,0),n,replace = TRUE)
+theta = rbind(theta1,theta2)
+x = rep(0,n)
+for(j in 1:n){
+  x[j] = pi[j] * rnorm(1,mean = theta1,sd = likelihood_var) + (1-pi[j]) * rnorm(1,mean = theta2,sd = likelihood_var) 
+}
+
 
 ## initialise parameters
-n = 100
+n = n
 x = x
 iter = 100
 likelihood_var = 1
 w = 0.5
 
-EP1 = EPGM(n = 100,x = x,iter = 400,likelihood_var = 1,w = 0.5)
-EP1
+EP1 = EPGM(n = n,x = x,iter = 100,likelihood_var = likelihood_var,w = 0.5)
+EP1$mu[,1:100]
+plot(EP1$mu[1,])
+EP1$Sigma[,1:200]
+## real posterior
+theta1s = seq(-5,5,length.out = 100)
+theta2s = seq(-5,5,length.out = 100)
+posterior = matrix(0,100,100)
+for(i in 1:100){
+  for(j in 1:100){
+    prior = dnorm(theta1s[i],0,1) * dnorm(theta2s[j],0,1)
+    likelihood = prod(w*dnorm(x,mean = theta1s[i],likelihood_var) + (1-w)*dnorm(x,mean = theta2s[j],likelihood_var))
+    posterior[i,j] = prior * likelihood
+  }
+}
 
+
+
+contour(theta1s,theta2s,posterior,color = 2)
+EPapproximate = matrix(0,nrow = 100,ncol = 100)
+for(i in 1:100){
+  for(j in 1:100){
+    EPapproximate[i,j] = dmvnorm(c(theta1s[i],theta2s[j]),mean = EP1$mu[,10],EP1$Sigma[,9:10])
+  }
+}
+contour(theta1s,theta2s,EPapproximate,color = 2,add = TRUE)
--- module6_vb.R
+++ module6_vb.R
@@ -1,27 +1,35 @@
 theta <- c(-1,1)
-sigma <- 1
+sigma <- 0.1
 n <- 100
 ind <- sample(c(1,2), n, replace = TRUE)
 data <- rnorm(n, sd = sigma) + theta[ind] 
 
-#prior 2-dim standard normal
-Q = matrix(0:1, nrow = 2, ncol = 2) 
-z = sample(c(1,2), n, replace = TRUE) #initialize labels
-Q_old = matrix(0, nrow = 2, ncol = 2)
-eps = 1e-3
 
-while(max(abs(Q - Q_old))>eps){
-  Q_old = Q
+
+VBMixture<-function(data, sigma,  eps = 1e-3){
   
-  Q[2,1] =  sigma^2/sum(z==1)/(1 + sigma^2/sum(z==1)) #var of Q1
-  Q[1,1] =  1/Q[2,1]*mean(data[z==1])/(1 + 1/Q[2,1]) #mean of Q1
+  #prior 2-dim standard normal
+  Q = matrix(0:1, nrow = 2, ncol = 2) 
+  colnames(Q)<-c("1st component","2nd component")
+  rownames(Q)<-c("PosteriorMean","PosteriorVar")
+  z = sample(c(1,2), n, replace = TRUE) #initialize labels
+  Q_old = matrix(0, nrow = 2, ncol = 2)
+
+  while(max(abs(Q - Q_old))>eps){
+    Q_old = Q
+    
+    Q[2,1] =  sigma^2/sum(z==1)/(1 + sigma^2/sum(z==1)) #var of Q1
+    Q[1,1] =  1/Q[2,1]*mean(data[z==1])/(1 + 1/Q[2,1]) #mean of Q1
+    
+    Q[2,2] =  sigma^2/sum(z==2)/(1 + sigma^2/sum(z==2)) #var of Q2
+    Q[1,2] =  1/Q[2,2]*mean(data[z==2])/(1 + 1/Q[2,2]) #mean of Q2
+    
+    #update labels
+    #cluster 2 if density of cluster 1 lower than that of cluster 2
+    z = as.numeric(dnorm(data, Q[1,1], sqrt(Q[2,1])) < dnorm(data, Q[1,2], sqrt(Q[2,2]))) + 1 
+  }
+  return(Q)
   
-  Q[2,2] =  sigma^2/sum(z==2)/(1 + sigma^2/sum(z==2)) #var of Q2
-  Q[1,2] =  1/Q[2,2]*mean(data[z==2])/(1 + 1/Q[2,2]) #mean of Q2
-  
-  #update labels
-  #cluster 2 if density of cluster 1 lower than that of cluster 2
-  z = as.numeric(dnorm(data, Q[1,1], sqrt(Q[2,1])) < dnorm(data, Q[1,2], sqrt(Q[2,2]))) + 1 
 }
 
 
