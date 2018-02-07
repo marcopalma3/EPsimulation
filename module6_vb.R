5641511ae3cff13d6aef3e84ee29c6e04ff03cd5
Fan Wu <fan.wu@spc.ox.ac.uk>
2018-02-07 13:36:29 +0000

    vb

---
+++ module6_vb.R
@@ -0,0 +1,27 @@
+theta <- c(-1,1)
+sigma <- 1
+n <- 100
+ind <- sample(c(1,2), n, replace = TRUE)
+data <- rnorm(n, sd = sigma) + theta[ind] 
+
+#prior 2-dim standard normal
+Q = matrix(0:1, nrow = 2, ncol = 2) 
+z = sample(c(1,2), n, replace = TRUE) #initialize labels
+Q_old = matrix(0, nrow = 2, ncol = 2)
+eps = 1e-3
+
+while(max(abs(Q - Q_old))>eps){
+  Q_old = Q
+  
+  Q[2,1] =  sigma^2/sum(z==1)/(1 + sigma^2/sum(z==1)) #var of Q1
+  Q[1,1] =  1/Q[2,1]*mean(data[z==1])/(1 + 1/Q[2,1]) #mean of Q1
+  
+  Q[2,2] =  sigma^2/sum(z==2)/(1 + sigma^2/sum(z==2)) #var of Q2
+  Q[1,2] =  1/Q[2,2]*mean(data[z==2])/(1 + 1/Q[2,2]) #mean of Q2
+  
+  #update labels
+  #cluster 2 if density of cluster 1 lower than that of cluster 2
+  z = as.numeric(dnorm(data, Q[1,1], sqrt(Q[2,1])) < dnorm(data, Q[1,2], sqrt(Q[2,2]))) + 1 
+}
+
+
